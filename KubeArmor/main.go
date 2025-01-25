4a76b70d53202066b9ee3f8e937144c87c38c705
Meet Soni <meetsoni3017@gmail.com>
2025-01-25 13:53:12 +0530

    this should fail

--- KubeArmor/main.go
+++ KubeArmor/main.go
@@ -38,7 +38,7 @@ func main() {
 	kg.Printf("Need to have root privileges to run %s\n", os.Args[0])
 	if os.Geteuid() != 0 {
 		if os.Getenv("KUBEARMOR_UBI") == "" {
-			kg.Printf("Need to have root privileges to run %s\n", os.Args[0])
+			kg.Printf("Need to have root privileges to run %s\n", os.Args[0]
 			return
 		}
 	}
